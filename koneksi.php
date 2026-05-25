f493ca424f7905686c86690d312a0b3bf3249657
mpspatasi <mpsp.atasi@gmail.com>
2026-05-25 13:10:12 +0700

    update konek

--- koneksi.php
+++ koneksi.php
@@ -1,8 +1,8 @@
 <?php
-$host = 'mysql://mysql:E9Kn79QRhLOkJ5BXKMWUprzhyarW0JLXmiwzZkSF8zESQbJ2UqiSdjdmoiAG21VZ@e6yyn6xv9yiprmm1esqltamu:3306/default';
-$db   = 'jurnal_transaksi_atasi';
-$user = 'root';
-$pass = 'CButdzzrksIHIcWtSki9kIG64an0F7fEV60xdgSfAuzizKxhCVnmueqagdIpp6qC';
+$host = 'e6yyn6xv9yiprmm1esqltamu';
+$db   = 'default';
+$user = 'mysql';
+$pass = 'E9Kn79QRhLOkJ5BXKMWUprzhyarW0JLXmiwzZkSF8zESQbJ2UqiSdjdmoiAG21VZ';
 $charset = 'utf8mb4';
 
 $dsn = "mysql:host=$host;port=3306;dbname=$db;charset=$charset";
@@ -11,9 +11,10 @@ $options = [
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
     PDO::ATTR_EMULATE_PREPARES   => false,
 ];
+
 try {
-     $pdo = new PDO($dsn, $user, $pass, $options);
+    $pdo = new PDO($dsn, $user, $pass, $options);
 } catch (\PDOException $e) {
-     throw new \PDOException($e->getMessage(), (int)$e->getCode());
+    throw new \PDOException($e->getMessage(), (int)$e->getCode());
 }
 ?>
